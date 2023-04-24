1b14f97cd542026b961e9d68cf9fab9b8ae376cd
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-04-24 11:50:11 +0300

    js+style

--- js/script.js
+++ js/script.js
@@ -9,6 +9,17 @@ let isMobile = {
 let body = document.querySelector('body');
 if(isMobile.any()){
     body.classList.add('touch');
+    let arrow = document.querySelectorAll('.arrow');
+    for(i = 0;i < arrow.length; i++){
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parent');
+        arrow[i].addEventListener('click', function(){
+            subMenu.classList.add('parent');
+        })
+    }
 } else{
     body.classList.add('mouse');
 }
